315ec4056f2acbce2defe0980df2bd8c12175040
Jean-Louis Ardoint <jeanlouis.ardoint@free.fr>
2024-09-25 11:02:14 +0200

    direct callbacks

--- moirai.cpp
+++ moirai.cpp
@@ -1,80 +1,106 @@
 #include <moirai.h>
 
-Moirai::Moirai(MoiraiCallback callback) {
-    this->callback = callback;
+
+
+Moirai::Moirai(MoiraiCallback callback) : 
+callbacks(Moirai::Callbacks(callback, callback, callback, callback, callback))
+{
     state = State::INIT;
-    notify();
+    callbacks.getInitCallback()(*this);
 }
 
-void Moirai::begin() {
-    if (state != State::BEGINNING && state != State::ONGOING) {
-      setPlannedEndTime(0);
-      state = State::BEGINNING;
-      notify();
-      state = State::ONGOING;
+Moirai::Moirai(MoiraiCallback initCallback,
+               MoiraiCallback beginningCallback,
+               MoiraiCallback ongoingCallback,
+               MoiraiCallback endingCallback,
+               MoiraiCallback stoppedCallback) : 
+               callbacks(Moirai::Callbacks(initCallback,
+                                           beginningCallback,
+                                           ongoingCallback,
+                                           endingCallback,
+                                           stoppedCallback))
+{
+    state = State::INIT;
+    callbacks.getInitCallback()(*this);
+}
+
+void Moirai::begin()
+{
+    if (state != State::BEGINNING && state != State::ONGOING)
+    {
+        setPlannedEndTime(0);
+        state = State::BEGINNING;
+        callbacks.getBeginningCallback()(*this);
+        state = State::ONGOING;
     }
 }
 
-void Moirai::begin(unsigned long duration) {
-    if (state != State::BEGINNING && state != State::ONGOING) {
-      setPlannedEndTime(millis() + duration);
-      state = State::BEGINNING;
-      notify();
-      state = State::ONGOING;
+void Moirai::begin(unsigned long duration)
+{
+    if (state != State::BEGINNING && state != State::ONGOING)
+    {
+        setPlannedEndTime(millis() + duration);
+        state = State::BEGINNING;
+        callbacks.getBeginningCallback()(*this);
+        state = State::ONGOING;
     }
 }
 
-void Moirai::checkTime() {
-    if (state == State::ONGOING 
-      && endTime != 0
-      && millis() >= endTime) {
+void Moirai::checkTime()
+{
+    if (state == State::ONGOING && endTime != 0 && millis() >= endTime)
+    {
         state = State::ENDING;
-        notify();
-     }
+        callbacks.getEndingCallback()(*this);
+    }
 }
 
-void Moirai::stop() {
+void Moirai::stop()
+{
     state = State::STOPPED;
-    notify();
+    callbacks.getStoppedCallback()(*this);
 }
 
-Moirai::State Moirai::getState() {
+Moirai::State Moirai::getState()
+{
     return state;
 }
 
-void Moirai::setPlannedEndTime(unsigned long endTime) {
+void Moirai::setPlannedEndTime(unsigned long endTime)
+{
     this->endTime = endTime;
 }
 
-unsigned long Moirai::getPlannedEndTime() {
+unsigned long Moirai::getPlannedEndTime()
+{
     return endTime;
 }
 
-void Moirai::notify() {
-    callback(*this);
-}
-
-PeriodicMoirai::PeriodicMoirai(MoiraiCallback callback, unsigned long periodInMs) 
-: Moirai(callback) {
+PeriodicMoirai::PeriodicMoirai(MoiraiCallback callback, unsigned long periodInMs)
+    : Moirai(callback)
+{
     this->periodInMs = periodInMs;
 }
 
-void PeriodicMoirai::begin() {
+void PeriodicMoirai::begin()
+{
     Moirai::begin();
-    cycleEnd = millis()+periodInMs;
+    cycleEnd = millis() + periodInMs;
 }
 
-
-void PeriodicMoirai::begin(unsigned long endTime) {
+void PeriodicMoirai::begin(unsigned long endTime)
+{
     Moirai::begin(endTime);
-    cycleEnd = millis()+periodInMs;
+    cycleEnd = millis() + periodInMs;
 }
 
-void PeriodicMoirai::checkTime() {
+void PeriodicMoirai::checkTime()
+{
     Moirai::checkTime();
     unsigned long currentTime = millis();
-     if (getState() == State::ONGOING && currentTime > cycleEnd) {
-         Moirai::notify();
-         cycleEnd = currentTime +periodInMs;
-    }       
+    if (getState() == State::ONGOING && currentTime > cycleEnd)
+    {
+        callbacks.getOngoingCallback()(*this);
+        cycleEnd = currentTime + periodInMs;
+    }
 }
